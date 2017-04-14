1ef38f8134132ddf5687e48891b054e8150f03b3
LionGS <gb1541@likelion.org>
2017-04-14 19:15:47 +0900

    error edit

---
+++ db/migrate/20170413100817_create_replies.rb
@@ -0,0 +1,10 @@
+class CreateReplies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :replies do |t|
+      t.text :content
+      t.integer :posts_model_id
+
+      t.timestamps
+    end
+  end
+end
